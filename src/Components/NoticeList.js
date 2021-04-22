83ea4de4652f3c304b67757c33c19ede8596c5c7
Inyeob Kim <deran2016@gmail.com>
2021-04-22 10:46:17 +0900

    Add Link in NoticeList.js

--- src/Components/NoticeList.js
+++ src/Components/NoticeList.js
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import { Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -14,7 +14,7 @@ function NoticeList() {
                 setNotices(null);
                 setLoading(true);
 
-                const response = await axios.get("http://localhost:8080/notice");
+                const response = await axios.get("http://localhost:8080/notice/" + 2);
                 setNotices(response.data);
             } catch (e) {
                 setError(e);
@@ -26,10 +26,15 @@ function NoticeList() {
     }, []);
 
     function tableDatas(notices) {
+        const url = "https://www.skku.edu/skku/campus/skk_comm/notice01.do"
         const datas = notices.map((notice) => 
             <TableRow key={notice.no}>
                 <TableCell align="center">{notice.no}</TableCell>
-                <TableCell align="left">{notice.title}</TableCell>
+                <TableCell align="left">
+                    <Link href={url + notice.href} color="inherit">
+                        {notice.title}
+                    </Link>
+                </TableCell>
                 <TableCell align="right">{notice.date}</TableCell>
                 <TableCell align="right">{notice.views}</TableCell>
             </TableRow>
@@ -55,7 +60,7 @@ function NoticeList() {
                     {tableDatas(notices)}
                 </TableBody>
             </Table>
-        </TableContainer>  
+        </TableContainer>
     );
 }
 
