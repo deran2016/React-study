c147d5092f83cbd6a1b5777865604191133f059c
Inyeob Kim <deran2016@gmail.com>
2021-04-22 11:25:59 +0900

    Add Paging in NoticeList

--- src/Components/NoticeList.js
+++ src/Components/NoticeList.js
@@ -1,4 +1,5 @@
-import { Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import { Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Typography, Box } from "@material-ui/core";
+import { ChevronLeft, ChevronRight } from "@material-ui/icons";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -6,6 +7,7 @@ function NoticeList() {
     const [notices, setNotices] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         const fetchNotices = async () => {
@@ -14,7 +16,7 @@ function NoticeList() {
                 setNotices(null);
                 setLoading(true);
 
-                const response = await axios.get("http://localhost:8080/notice/" + 2);
+                const response = await axios.get("http://localhost:8080/notice/" + page);
                 setNotices(response.data);
             } catch (e) {
                 setError(e);
@@ -23,7 +25,7 @@ function NoticeList() {
             }
         }
         fetchNotices();
-    }, []);
+    }, [page]);
 
     function tableDatas(notices) {
         const url = "https://www.skku.edu/skku/campus/skk_comm/notice01.do"
@@ -42,25 +44,46 @@ function NoticeList() {
         return datas;
     }
 
+    function goPrevPage() {
+        if (page > 1) setPage(page - 1);
+    }
+
+    function goNextPage() {
+        setPage(page + 1);
+    }
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error!</p>;
     if (!notices) return null;
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center">NO</TableCell>
-                        <TableCell align="center">제목</TableCell>
-                        <TableCell align="right">날짜</TableCell>
-                        <TableCell align="right">조회수</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tableDatas(notices)}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <Box align="center">
+                <IconButton display="inline" onClick={goPrevPage}>
+                    <ChevronLeft></ChevronLeft>
+                </IconButton>
+                <Typography variant="body1" display="inline">
+                    {page}
+                </Typography>
+                <IconButton display="inline" onClick={goNextPage}>
+                    <ChevronRight></ChevronRight>
+                </IconButton>
+            </Box>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">NO</TableCell>
+                            <TableCell align="center">제목</TableCell>
+                            <TableCell align="right">날짜</TableCell>
+                            <TableCell align="right">조회수</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {tableDatas(notices)}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     );
 }
 
