5cbaa1f8502209ae0f4f8a6471cfe4609960a3eb
Inyeob Kim <deran2016@gmail.com>
2021-04-22 10:45:43 +0900

    Add material-ui in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import './App.css';
-import Clock from './Components/Clock';
 import ClockF from './Components/ClockF';
-import Toggle from './Components/Toggle';
 import ToggleF from './Components/ToggleF';
-import List from './Components/List';
 import UserList from './Components/UserList';
 import NoticeList from './Components/NoticeList';
 import '@fontsource/roboto';
+import { AppBar, Container, IconButton, Toolbar } from '@material-ui/core';
 
 function App() {
-  const numbers = [1, 2, 3, 4, 5];
   const users = [{id: "1", name: "Kim In Yeob", age: 24, sex: "남"}];
 
   return (
     <div>
-      <Clock></Clock>
-      <ClockF></ClockF>
-      <Toggle></Toggle>
-      <ToggleF></ToggleF>
-      <List numbers={numbers}></List>
-      <UserList users={users}></UserList>
-      <NoticeList></NoticeList>
+      <Container maxWidth="sm">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton>
+              
+            </IconButton>
+            <ClockF></ClockF>
+          </Toolbar>
+        </AppBar>
+        <ToggleF></ToggleF>
+        <UserList users={users}></UserList>
+        <NoticeList></NoticeList>
+      </Container>
     </div>
   );
 }
